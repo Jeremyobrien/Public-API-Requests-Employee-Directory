32f046baeeb89bc5fb5cdf1fa2340fb18217401e
@Jeremyobrien <jjob1391@gmail.com>
2021-09-24 00:30:59 -0400

    fix async call to generateCards()

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 
 const directoryPage =  document.querySelector('#gallery');
-const employeeList = document.createElement('ul');
-directoryPage.appendChild(employeeList);
+const employeeDataUrl = 'https://randomuser.me/api/?results=12';
+
+
+
 
 //Fetch functions
-function fetchData(url) {
-    return fetch(url)
-            .then(checkStatus)
-            .then(res => res.json())
-            .catch(error => console.log('Looks like there was a problem', error))
+async function fetchData(url) {
+   let employeeInfo = await fetch(url)
+                                .then(checkStatus)
+                                .then(res => res.json())
+                                .catch(error => console.log(Error(error)))
+    employeeInfo = employeeInfo.results;
+    return employeeInfo;
 }
-
-fetchData('https://randomuser.me/api/?results=12&inc=name,location,email,picture,cell')
-    .then(res => {
-        const employeeInfo = res.results;
-       return generateCards(employeeInfo);
-    })
+    
 
 //Helper Functions
 function checkStatus(response){
@@ -27,8 +26,7 @@ function checkStatus(response){
 }
 
 function generateCards (arr) {
-    const cards = arr.map(card => `
-    <li>
+    const cards = arr.map(card => ` 
     <div class="card">
     <div class="card-img-container">
         <img class="card-img" src="${card.picture.large}" alt="profile picture">
@@ -40,15 +38,49 @@ function generateCards (arr) {
     </div>
 </div>
 </div>
-</li>
-`)
-employeeList.insertAdjacentHTML('beforeend', cards); 
-return employeeList.insertAdjacentHTML('afterend', '</ul>');
+`).join('');
+return directoryPage.insertAdjacentHTML('beforeend', cards); 
 };
 
+function generateModal(employee) {
+    const modal =`<div class="modal-container">
+                    <div class="modal">
+                        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                        <div class="modal-info-container">
+                            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+                            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                            <p class="modal-text">${employee.email}</p>
+                            <p class="modal-text cap">${employee.location.city}</p>
+                            <hr>
+                            <p class="modal-text">${employee.cell}</p>
+                            <p class="modal-text">${employee.street.number} ${employee.street.name}, ${employee.street.city}, ${employee.state} ${employee.postcode}</p>
+                            <p class="modal-text">Birthday: ${employee.dob.date}</p>
+                        </div>
+                    </div>
+
+                    // IMPORTANT: Below is only for exceeds tasks 
+                    <div class="modal-btn-container">
+                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                    </div>
+                </div>`;
+            
+     return modal;  
+    }
+
+fetchData(employeeDataUrl)                        
+    .then(generateCards)
+    .catch(error => console.log(Error(error)))
+
 //Event listeners
-document.addEventListener('load', async ()=>{
-    const data =  await fetchData();
-    const cards = await generateCards(data)
-    return cards;
- })
+
+    directoryPage.addEventListener('click', async (e) => {
+        const cards = directoryPage.children;
+        const employee = e.target;
+        for(let i = 1; i < cards.length; i++) {
+            if (employee.classList.contains('card') && cards[i] === employee){
+                const popup  = await generateModal(employee);
+                popup.style.display = 'block';
+            }
+    }
+})
